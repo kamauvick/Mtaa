2d6cead712ad54997b141e8ad925b46b5dfeb027
vick3445 <kamauvick@outlook.com>
2018-11-21 09:28:41 +0300

    #

--- app/src/main/java/com/example/vick/mtaa/LoginActivity.java
+++ app/src/main/java/com/example/vick/mtaa/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.example.vick.mtaa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button signupbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signupbtn = findViewById(R.id.signupbtn);
+        goToNext();
     }
+
+    public void goToNext(){
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
 }
